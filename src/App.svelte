5a1794705ee2c7212f0dfb48b002b2e18afb34a7
amalshaji <amalshajid@gmail.com>
2021-01-11 15:07:53 +0530

    added link to github

--- src/App.svelte
+++ src/App.svelte
@@ -56,7 +56,14 @@
 	<div class="container mt-5">
 		<div class="row">
 			<div class="col-md-6 m-auto">
-				<h3 class="text-center mb-3">Public API search</h3>
+				<h3 class="text-center mb-3">
+					Public API Search
+					<a
+						href="https://github.com/amalshaji/apisearch"
+						target="_blank"><img
+							src="./github.svg"
+							alt="github" /></a>
+				</h3>
 				<div class="form-group">
 					<input
 						type="text"
